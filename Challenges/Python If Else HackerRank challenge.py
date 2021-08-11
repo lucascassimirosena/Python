7450f3f08dad7a5103d127f275ea53122a092bfc
Lucas Cassimiro Sena Colombo <67432077+lucascassimirosena@users.noreply.github.com>
2021-08-11 16:04:34 -0300

    If else challenge solved

--- Challenges/Python If Else HackerRank challenge.py
+++ Challenges/Python If Else HackerRank challenge.py
@@ -1,17 +1,17 @@
-
-n = float(input(""))
-
-
-
-
-if (n%2) == 0:
-    if n > 2 <= 5:
-        print("Not Weird")
-    elif n >= 6 <= 20:
-        print("Weird")
-
-else:
-
-    print("Weird")
+import math
+import os
+import random
+import re
+import sys
 
 
+
+if __name__ == '__main__':
+    n = int(raw_input().strip())
+    check = {True: "Not Weird", False: "Weird"} #Boolean values True or False, it'll print a message depending
+    #on the value
+    print(check[                #this  will start and print the value of the variable
+        n%2==0 and (            #if the value meets these specification it will be true and not weird
+            n in range(2, 6) or
+            n > 20)
+    ])
