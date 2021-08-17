d2171a7e3f0fe2c6993d35031d61f421a461be04
Lucas Cassimiro Sena Colombo <67432077+lucascassimirosena@users.noreply.github.com>
2021-08-16 21:17:52 -0300

    Learned about String Replacement Fields in Python

---
+++ Studies/StringReplacementFields.py
@@ -0,0 +1,11 @@
+# Numbers can't be concatenated withstring using +, Python will attempt
+# addition and then fail.
+# In python every data type can be coerced into a string representation
+
+age = 28
+print("My age is {0} years" .format(age))
+
+print("There are {0} days in {1}, {2}, {3}, {4}, {5}, {6} and {7}"
+    .format(31, "Jan", "Mar", "May, Jul, Aug, Oct, Dec"))
+
+
