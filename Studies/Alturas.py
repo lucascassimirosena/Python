cb7420e0072820e6bc08bb12fd6edafac896ee6a
Lucas Cassimiro Sena Colombo <67432077+lucascassimirosena@users.noreply.github.com>
2021-10-25 13:04:37 -0300

    training

--- Studies/Alturas.py
+++ Studies/Alturas.py
@@ -1,5 +1,7 @@
 # Just a simple app that scans the height of the person inside an array.
 # This app was created as an exercise in the Universidade Anhembi Morumbi
+# After finishing and sending the activity I decided to keep modifying it to traine
+
 
 
 alturas = [1.80, 1.73, 1.81, 1.85]
@@ -9,4 +11,15 @@ for altura in alturas:
         print("Entrada permitida")
     else:
         print("Entrada não permitia")
+
+weights = [40, 50, 50, 70, 80, 90]
+
+for weight in weights:
+    if weight <= 40:
+        print("Entrada nao permitida")
+    elif weight > 40 < 70:
+        print("Entrada permitida")
+    else:
+        print("Entrada nao permitida")
+
 
