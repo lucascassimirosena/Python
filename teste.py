2e6bdf692be9bd77993a623a82e1449b8b953871
Lucas Cassimiro <lucas.cassimiro@live.com>
2023-02-03 02:16:11 -0300

    test

---
+++ teste.py
@@ -0,0 +1,13 @@
+# I decided to study Python again since it's one of the languages I want to learn. 
+
+# Function print - It prints... lol
+print("First lesson: ")
+print("Hello World")
+
+
+# -----> """ this is a docstring, it means I can make multiple comments
+
+"""" Now it's possible to make a multiple line comment
+it's not the same kind of comment as the "#' is. """
+
+
