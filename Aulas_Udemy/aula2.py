02822839d32fb4a5cd3c9be232e6770b7a0a7088
Lucas Cassimiro <lucas.cassimiro@live.com>
2023-03-11 18:23:00 -0300

    back to the studies

---
+++ Aulas_Udemy/aula2.py
@@ -0,0 +1,24 @@
+print(12, 34, sep='-', end='\r\n')
+print(56, 78, sep="-")
+
+#sep descreve o separador
+#end o que acontece no final
+# \r\n - quebra de linha
+
+""""
+Python - Linguagem de programação
+Tipo de linguagem = Dinâmica / Forte
+str -> string -> texto
+Strings são textos que estão dentro de aspas
+"""
+
+# Aspas duplas
+print("Lucas Sena")
+print(1, 'Lucas "Sena"')
+print(2, "Lucas 'Sena'")
+
+# Escape
+print("Lucas \"Sena\"")
+
+# r
+print(r"Lucas \"Sena\"")
